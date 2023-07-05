2682ea5a8ade441fd1028cc345a2a26e3046b4e3
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-05 01:55:00 -0300

    router test

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react'
+
+const layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div><h2>About layout</h2>{children}</div>
+  )
+}
+
+export default layout
---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const metadata = {
+  title: "About",
+};
+
+const Page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Page;
