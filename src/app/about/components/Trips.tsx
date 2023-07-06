0522592b8001cb4f5472b1050e7e6f00d8d9092c
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-05 23:49:33 -0300

    convert string

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,10 +1,22 @@
+import { prisma } from '@/lib/prisma';
 import React, { useState } from 'react'
 
-const Trips = () => {
-  
+const getTrips = async() => {
+  const trips = await prisma.trip.findMany({});
+
+  return trips;
+};
+
+const Trips = async () => {
+  const data = await fetch("http://jsonplaceholder.typicode.com/posts").then((res) => res.json());
+
   return (
-    <div>Trips</div>
-  )
+  <div>
+      {data.map((i: any) => (
+      <p key={i.id}>{i.title}</p>
+      ))}
+      </div>
+  );
 }
 
-export default Trips
+export default Trips;
