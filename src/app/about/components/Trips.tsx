d6d81908c4e7f797a40993da2e11e0b9c8f7482d
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-06 00:24:11 -0300

    trip attt

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,24 +1,24 @@
-"use client"
+import { prisma } from "@/lib/prisma";
+import React, { useState } from "react";
 
-import { prisma } from '@/lib/prisma';
-import React, { useState } from 'react'
-
-const getTrips = async() => {
+const getTrips = async () => {
   const trips = await prisma.trip.findMany({});
 
   return trips;
 };
 
 const Trips = async () => {
-  const data = await fetch("http://jsonplaceholder.typicode.com/posts").then((res) => res.json());
+  const data = await fetch("http://jsonplaceholder.typicode.com/posts").then(
+    (res) => res.json()
+  );
 
   return (
-  <div>
+    <div>
       {data.map((i: any) => (
-      <p key={i.id}>{i.title}</p>
+        <p key={i.id}>{i.title}</p>
       ))}
-      </div>
+    </div>
   );
-}
+};
 
 export default Trips;
