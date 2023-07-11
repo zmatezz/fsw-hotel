9cb41767aecd492adf98f3302699e3f5ae9f0d94
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-11 00:35:46 -0300

    auth working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-import Image from 'next/image';
-import { Inter } from "next/font/google";
+"use client";
+
+import { signIn, signOut, useSession } from 'next-auth/react'
  
 export default function Home() {
+  const {data} = useSession()
+
+  data?.user
+
   return (
-    <h1>Home Page</h1>
-  )
+    <div>
+      <div className="flex flex-col">
+      <button onClick={() => signIn()}>Login</button>
+      <button onClick={() => signOut()}>Logout</button>
+      </div>
+
+      <h1>Olá, {data?.user?.name}</h1>
+      <img src={data?.user?.image ?? ""} alt="" />
+    </div>
+  );
 }
