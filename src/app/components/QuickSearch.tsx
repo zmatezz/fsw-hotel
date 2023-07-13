304730dcb1d467c75c48da46f2431a27a4c3f345
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-13 00:45:09 -0300

    try search

---
+++ src/app/components/QuickSearch.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const QuickSearch = () => {
+  return (
+    <div className='container mx-auto p-5'>
+      <div className="flex items-center">
+        <div className="w-full h-[1px] bg-grayLighter"></div>
+        <h2 className='px-5 font-medium text-grayPrimary whitespace-nowrap'>Tente pesquisar por</h2>
+        <div className="w-full h-[1px] bg-grayLighter"></div>
+
+      </div>
+    </div>
+  )
+}
+
+export default QuickSearch
