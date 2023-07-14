7ab415010bb17df761d8874755f50c9be26ddd76
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-14 00:28:02 -0300

    trip att

--- src/app/components/RecommendedTrips.tsx
+++ src/app/components/RecommendedTrips.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const RecommendedTrips = async () => {
-  const data = await fetch("http://localhost:3000/hello").then(res => res.json()); 
+  const data = await fetch("http://localhost:3000/hello").then((res) =>
+    res.json()
+  );
 
-  return <div className="container mx-auto p-5">
-    <div className="flex items-center">
+  return (
+    <div className="container mx-auto p-5">
+      <div className="flex items-center">
         <div className="w-full h-[1px] bg-grayLighter"></div>
         <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">
           Destinos recomendados
@@ -12,8 +15,11 @@ const RecommendedTrips = async () => {
         <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
 
-      {data.map((trip) => (<img key={trip.id} src={trip.coverImage} alt="asas" />))}
-  </div>;
+      {data.map((trip) => (
+        <img key={trip.id} src={trip.coverImage} alt="asas" />
+      ))}
+    </div>
+  );
 };
 
 export default RecommendedTrips;
