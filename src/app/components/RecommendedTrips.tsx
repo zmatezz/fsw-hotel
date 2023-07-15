fa4c44580ad32509d09ead7151b196bb153162a0
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-14 21:16:12 -0300

    loading fix

--- src/app/components/RecommendedTrips.tsx
+++ src/app/components/RecommendedTrips.tsx
@@ -1,11 +1,17 @@
 import TripItem from "@/components/TripItem";
+import { prisma } from "@/lib/prisma";
 import { Trip } from "@prisma/client";
+import { data } from "autoprefixer";
 import React from "react";
 
+async function getTrips() {
+  const trips = await prisma.trip.findMany({});
+  
+  return trips;
+}
+
 const RecommendedTrips = async () => {
-  const data = await fetch("http://localhost:3000/hello").then((res) =>
-    res.json()
-  );
+  const data = await getTrips();
 
   return (
     <div className="container mx-auto p-5">
@@ -18,9 +24,9 @@ const RecommendedTrips = async () => {
       </div>
 
       <div className="flex flex-col items-center mt-5 gap-5">
-      {data.map((trip: Trip) => (
-       <TripItem key={trip.id} trip={trip}/>
-      ))}
+        {data.map((trip: Trip) => (
+          <TripItem key={trip.id} trip={trip} />
+        ))}
       </div>
     </div>
   );
