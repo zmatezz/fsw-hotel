ccf3a0c22c098bbdac96ca3f5f12d55bc522e3a7
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-17 00:13:17 -0300

    create TripHeader component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 import React from "react";
 import ReactCountryFlag from "react-country-flag";
+import TripHeader from "./components/TripHeader";
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -22,32 +23,9 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
 
   return (
     <div className="container mx-auto">
-      <div className="relative h-[280px] w-full">
-        <Image
-          src={trip.coverImage}
-          fill
-          style={{ objectFit: "cover" }}
-          alt={trip.name}
-        />
-      </div>
 
-      <div className="flex flex-col p-5">
-        <h1 className="font-semibold text-xl text-primaryDarker">
-          {trip.name}
-        </h1>
+    <TripHeader trip={trip}/>
 
-        <div className="flex items-center gap-1 my-1">
-          <ReactCountryFlag countryCode={trip.countryCode} svg />
-          <p className="text-xs text-grayPrimary underline">{trip.location}</p>
-        </div>
-
-        <p className="text-xs text-grayPrimary">
-          <span className="text-primary font-medium">
-            R${trip.pricePerDay.toString()}
-          </span>{" "}
-          por dia.
-        </p>
-      </div>
     </div>
   );
 };
