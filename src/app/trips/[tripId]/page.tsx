b599a7e220ddc727a172cb0f28321192455821db
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-17 23:56:25 -0300

    feat: add trip highlight

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -5,6 +5,7 @@ import ReactCountryFlag from "react-country-flag";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
 import TripDescription from "./components/TripDescription";
+import TripHighlights from "./components/TripHighlights";
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -29,6 +30,7 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
     <TripHeader trip={trip}/>
     <TripReservation trip={trip}/>
     <TripDescription description={trip.description}/>
+    <TripHighlights highlights={trip.highlights}/>
     
     </div>
   );
