225b3f77ec6d8e87e0dcee92b5d89125e397ad07
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-18 00:23:04 -0300

    feat: add Button TripLocation

--- src/app/trips/[tripId]/components/TripLocation.tsx
+++ src/app/trips/[tripId]/components/TripLocation.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import Image from "next/image";
 import React from "react";
 
@@ -24,6 +25,7 @@ const TripLocation = ({ location, locationDescription }: TripLocationProps) => {
         {location}
       </h3>
       <p className="text-xs text-primaryDarker mt-2 leading-5">{locationDescription}</p>
+      <Button className="w-full mt-5">Ver no Google Maps</Button>
     </div>
   );
 };
