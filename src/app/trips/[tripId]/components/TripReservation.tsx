c97730f893b4f28c61a760d36ae9154efa0d032e
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-18 21:43:07 -0300

    feat: guests number validate

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -5,40 +5,69 @@ import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
 import { Trip } from "@prisma/client";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 interface TripReservationProps {
-  trip: Trip
+  trip: Trip;
+}
+
+interface TripReservationForm {
+  guests: number;
 }
 
 const TripReservation = ({ trip }: TripReservationProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TripReservationForm>();
+
+  const onSubmit = (data: any) => {
+    console.log({ data });
+  };
+
   return (
-
-      <div className="flex flex-col px-5">
-        <div className="flex gap-4">
-          <DatePicker
-            placeholderText="Data de Início"
-            onChange={() => {}}
-            className="w-full"
-          />
-          <DatePicker
-            placeholderText="Data Final"
-            onChange={() => {}}
-            className="w-full"
-          />
-        </div>
-
-        <Input placeholder={`Número de Hóspedes (max: ${trip.maxGuests})`} className="mt-4" />
-
-        <div className="flex justify-between mt-3">
-          <p className="font-medium text-sm text-primaryDarker">Total: </p>
-          <p className="font-medium text-sm text-primaryDarker">R$2500 </p>
-        </div>
-
-        <div className="pb-10 border-b border-grayLighter w-full">
-        <Button className="mt-3 w-full">Reservar agora</Button>
-        </div>
+    <div className="flex flex-col px-5">
+      <div className="flex gap-4">
+        <DatePicker
+          placeholderText="Data de Início"
+          onChange={() => {}}
+          className="w-full"
+        />
+        <DatePicker
+          placeholderText="Data Final"
+          onChange={() => {}}
+          className="w-full"
+        />
       </div>
- 
+
+      <Input
+        {...register("guests", {
+          required: {
+            value: true,
+            message: "Número de hóspedes é obrigatório",
+          },
+        })}
+        placeholder={`Número de Hóspedes (max: ${trip.maxGuests})`}
+        className="mt-4"
+        error={!!errors?.guests}
+        errorMessage={errors?.guests?.message}
+      />
+
+      <div className="flex justify-between mt-3">
+        <p className="font-medium text-sm text-primaryDarker">Total: </p>
+        <p className="font-medium text-sm text-primaryDarker">R$2500 </p>
+      </div>
+
+      <div className="pb-10 border-b border-grayLighter w-full">
+        <Button
+          onClick={() => handleSubmit(onSubmit)()}
+          className="mt-3 w-full"
+        >
+          Reservar agora
+        </Button>
+      </div>
+    </div>
   );
 };
 
