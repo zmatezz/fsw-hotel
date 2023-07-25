a23ab0575f725551147d80582b966d22efc00f1e
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-25 01:15:39 -0300

    feat: add route to get user reservations

---
+++ src/app/api/user/[userId]/reservations/route.ts
@@ -0,0 +1,35 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params: { userId } }: { params: { tripId: string } }
+) {
+  const { searchParams } = new URL(request.url);
+
+  console.log({ userId });
+
+  if (!userId) {
+    return {
+      status: 400,
+      body: {
+        message: "Missing userId",
+      },
+    };
+  }
+
+  const reservations = await prisma.tripReservation.findMany({
+    where: {
+      userId: userId,
+    },
+    include: {
+      trip: true,
+    },
+  });
+
+  console.log({ reservations });
+
+  return new NextResponse(JSON.stringify(reservations), {
+    status: 200,
+  });
+}
