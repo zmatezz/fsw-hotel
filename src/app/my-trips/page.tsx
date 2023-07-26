76a18b4960d3f40869ff7a41aa16546e3cd2843d
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-26 00:58:52 -0300

    feat: added new content in my trips(no trips)

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -5,13 +5,15 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { Prisma, TripReservation } from "@prisma/client";
 import UserReservationItem from "./components/UserReservationItem";
+import Button from "@/components/Button";
+import Link from "next/link";
 
 const MyTrips = () => {
-  const [reservations, setReservations] =
-    useState <
+  const [reservations, setReservations] = useState<
     Prisma.TripReservationGetPayload<{
       include: { trip: true };
-    }>[]>([]);
+    }>[]
+  >([]);
 
   const { status, data } = useSession();
 
@@ -41,9 +43,22 @@ const MyTrips = () => {
       <h1 className="font-semibold text-primaryDarker text-xl">
         Minhas Viagens
       </h1>
-         {reservations?.map((reservation) => (
-        <UserReservationItem key={reservation.id} reservation={reservation} />
-      ))}
+      {reservations.length > 0 ? (
+        reservations?.map((reservation) => (
+          <UserReservationItem key={reservation.id} reservation={reservation} />
+        ))
+      ) : (
+        <div className="flex flex-col">
+          <p className="mt-5 font-medium text-prim">
+          Você ainda não tem nenhuma reserva! =({" "}
+        </p>
+
+        <Link href="/">
+        <Button className="w-full mt-2">Fazer reserva!</Button>
+        </Link>
+
+        </div>
+      )}
     </div>
   );
 };
