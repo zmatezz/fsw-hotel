99153f597c30a719ff43279312108368d6058d3d
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-07-25 23:46:08 -0300

    feat: added date, guests in trip info

--- src/app/my-trips/components/UserReservationItem.tsx
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -1,4 +1,7 @@
+import Button from "@/components/Button";
 import { Prisma } from "@prisma/client";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import Image from "next/image";
 import React from "react";
 import ReactCountryFlag from "react-country-flag";
@@ -12,28 +15,50 @@ interface UserReservationItemProps {
 const UserReservationItem = ({ reservation }: UserReservationItemProps) => {
   const { trip } = reservation;
   return (
-    <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
-      <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
-        <div className="relative h-[106px] w-[124px]">
-          <Image
-            src={trip.coverImage}
-            fill
-            style={{ objectFit: "cover" }}
-            className="rounded-lg"
-            alt={trip.name}
-          />
-        </div>
-        <div className="flex flex-col">
-          <h2 className="text-xl text-primaryDarker font-semibold">
-            {trip.name}
-          </h2>
+    <div>
+      <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
+        <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
+          <div className="relative h-[106px] w-[124px]">
+            <Image
+              src={trip.coverImage}
+              fill
+              style={{ objectFit: "cover" }}
+              className="rounded-lg"
+              alt={trip.name}
+            />
+          </div>
+          <div className="flex flex-col">
+            <h2 className="text-xl text-primaryDarker font-semibold">
+              {trip.name}
+            </h2>
 
+            <div className="flex items-center gap-1">
+              <ReactCountryFlag countryCode={trip.countryCode} svg />
+              <p className="text-xs text-grayPrimary underline">
+                {trip.location}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-flex col mt-5 text-primaryDarker">
+          <h3 className="text-sm">Data</h3>
           <div className="flex items-center gap-1">
-            <ReactCountryFlag countryCode={trip.countryCode} svg />
-            <p className="text-xs text-grayPrimary underline">
-              {trip.location}
+            <p className="text-sm">
+              {format(new Date(reservation.startDate), "dd 'de' MMMM", {
+                locale: ptBR,
+              })}
+            </p>
+            {" - "}
+            <p className="text-sm">
+              {format(new Date(reservation.endDate), "dd 'de' MMMM", {
+                locale: ptBR,
+              })}
             </p>
           </div>
+
+          <h3 className="mt-5 text-sm">Hóspedes</h3>
+          <p className="text-sm">{reservation.guests}</p>
         </div>
       </div>
     </div>
