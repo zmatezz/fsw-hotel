24f969b6388e78916e5ac77b7594a59ea477c40c
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-08-01 21:44:11 -0300

    style: responsive search desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -30,16 +30,16 @@ const Trips = () => {
 
   return (
     <div className="container mx-auto flex flex-col items-center p-5">
-      <h1 className="text-primaryDarker font-semibold text-xl">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:text-[2.5rem]">
         Viagens Encontradas
       </h1>
-      <h2 className="text-grayPrimary font-medium mb-5">
+      <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6">
         {trips.length > 0
           ? "Listamos as melhores viagens pra você!"
           : "Não encontramos nada nos seus parâmetros! =("}
       </h2>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10  lg:pb-16">
         {trips?.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
